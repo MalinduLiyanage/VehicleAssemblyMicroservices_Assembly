5a2a73a8000450045318916af9c76f931dc18514
Malindu Liyanage <136006504+MalinduLiyanage@users.noreply.github.com>
2025-03-17 16:14:47 +0530

    API Requests for Accounts

--- obj/Debug/net8.0/AssemblyService.AssemblyInfo.cs
+++ obj/Debug/net8.0/AssemblyService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssemblyService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b5b5c6d628063b33347c87e073bfbc8f6eac51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0818ae34d9979994ce2833fd0406dafb02a789b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssemblyService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssemblyService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
